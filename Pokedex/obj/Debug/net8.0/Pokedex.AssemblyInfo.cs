d625b9b012d3888c903f1bd5ef28a03c0c54fefb
JasonLuis <jason20luis@gmail.com>
2025-03-03 15:26:23 -0300

    feat: cria pagina Info

--- Pokedex/obj/Debug/net8.0/Pokedex.AssemblyInfo.cs
+++ Pokedex/obj/Debug/net8.0/Pokedex.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+808c695945535be234c6bdd584947b168c0eef8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a64a38b60ca19a023e53097f32a205b2094f7586")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
