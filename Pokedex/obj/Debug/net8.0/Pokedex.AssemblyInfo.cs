590c95a7d149d8c4499e15322e5343f61b1988a4
JasonLuis <jason20luis@gmail.com>
2025-03-08 20:11:45 -0300

    refactor: altera o nome da secret

--- Pokedex/obj/Debug/net8.0/Pokedex.AssemblyInfo.cs
+++ Pokedex/obj/Debug/net8.0/Pokedex.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39ba569a5740e9ce3fc179a15ca1ed9f356d26df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ec93096385cd913eb9bdfe529fa8d29250fead8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
